95edf0f48efd45606376a84fb0b6a21797f82d5b
StatkevichVlad <statkevichvlad@gmail.com>
2022-09-03 19:34:49 +0300

    03.09

---
+++ tasks/takeATenMinutesWalk/Main.java
@@ -0,0 +1,8 @@
+package tasks.takeATenMinutesWalk;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(TakeATenMinutesWalk.isValid(new char[] {'n','s','n','s','n','s','n','s','n','s','s'}));
+
+    }
+}
